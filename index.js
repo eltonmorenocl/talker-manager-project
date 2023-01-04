485b9321ff6668ad98f8261c4ea8b5ee973cfa8a
elton <eltonmorenocl@gmail.com>
2023-01-04 17:25:22 -0300

    feat(swagger): adiciona documentação da api com o swagger

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const swaggerUi = require('swagger-ui-express');
 const express = require('express');
 const talkerRoute = require('./routes/talker');
 const loginRoute = require('./routes/login');
+
+const swaggerDocs = require('./swagger.json');
 
 const app = express();
 app.use(express.json());
@@ -13,6 +16,7 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/login', loginRoute);
 app.use('/talker', talkerRoute);
 
